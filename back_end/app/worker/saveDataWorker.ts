e1dfa91a3b9b018533889e91b906beccfc644750
Sherlock <1075773551@qq.com>
2024-10-26 13:40:06 +0800

    1. 新增时间步长，并且取整  2. 设置数据队列    3. 添加去重机制

--- back_end/app/worker/saveDataWorker.ts
+++ back_end/app/worker/saveDataWorker.ts
@@ -1,26 +1,84 @@
-import { Sequelize } from "sequelize-typescript";
-import DataModel, { IData } from "../model/Data.model";
+import {Sequelize} from "sequelize-typescript";
+import DataModel, {IData} from "../model/Data.model";
 import DataService from "../service/DataService";
 
-process.on('message', async (data: IData[]) => {
-  // 初始化 Sequelize 实例
-  const sequelize = new Sequelize({
-    dialect: 'mysql',    // 数据库类型
-    host: 'localhost',   // 数据库地址
-    username: 'root',    // 数据库用户名
-    password: 'root',// 数据库密码
-    database: 'vehicle' // 数据库名称
-  });
+// 初始化 Sequelize 实例
+const sequelize = new Sequelize({
+  dialect: 'mysql',    // 数据库类型
+  host: 'localhost',   // 数据库地址
+  username: 'root',    // 数据库用户名
+  password: 'root',    // 数据库密码
+  database: 'vehicle'  // 数据库名称
+});
 
-  // 手动注册模型
-  sequelize.addModels([DataModel]);
+// 手动注册模型
+sequelize.addModels([DataModel]);
+
+// 消息队列
+const messageQueue: IData[][] = [];
+let isProcessing = false;
+
+const getRoundedTimeStep = (timeStep: number) => {
+  return Math.pow(10, Math.round(Math.log10(timeStep )));
+}
+
+const manageData = (data: IData[]): IData[] => {
+  // 对于每一个signalId的数据，取相同信号id的最后一个时间戳和第一个时间戳，然后根据时间戳的差值，取最接近的时间戳
+  const signalIds = Array.from(new Set(data.map(item => item.signalId)));
+  const result: IData[] = [];
+  for (const signalId of signalIds) {
+    const arr = data.filter(item => item.signalId === signalId);
+    const length = arr.length;
+    let timeStep = (arr[length - 1].time - arr[0].time) / (length + 1);
+    timeStep = getRoundedTimeStep(timeStep);
+    let time = arr[length - 1].time;
+    for (let i = length - 1; i >= 0; i--) {
+      result.unshift({
+        ...arr[i],
+        time
+      });
+      time -= timeStep;
+    }
+  }
+  return result;
+}
+
+// store the unique key for arr
+const processedKeys = new Set<string>();
+const UNIQUEKEY_LIMIT = 100_000
+
+process.on('message', (data: IData[]) => {
+  const answer = manageData(data);
+  messageQueue.push(answer);
+  processQueue();
+});
+
+async function processQueue() {
+  if (isProcessing || messageQueue.length === 0) {
+    return;
+  }
+
+  isProcessing = true;
+  const data = messageQueue.shift() ?? [];
+
+  const uniqueData = data!.filter(item => {
+    const key = `${item.signalId}-${item.time}-${item.value}`;
+    if (processedKeys.has(key)) {
+      return false;
+    }
+    processedKeys.add(key);
+    if (processedKeys.size > UNIQUEKEY_LIMIT) {
+      processedKeys.clear();
+    }
+    return true;
+  });
 
   try {
     // 确保数据库同步
     await sequelize.sync();
 
     // 调用 DataService 的 addData 方法
-    await DataService.addData(data);
+    await DataService.addData(uniqueData!);
 
     // 向主进程发送成功消息
     // @ts-ignore
@@ -31,7 +89,7 @@ process.on('message', async (data: IData[]) => {
     process.send('error');
     console.error(error);
   } finally {
-    // 关闭 Sequelize 连接
-    await sequelize.close();
+    isProcessing = false;
+    processQueue();
   }
-});
+}
